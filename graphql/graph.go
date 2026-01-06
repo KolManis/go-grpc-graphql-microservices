6e4b5b281bdebad24ad09a3437618fd71cdc1c81
KolManis <nikola.ilyin03@yandex.ru>
2026-01-06 17:17:20 +0500

    Resolvers

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,10 +1,12 @@
 package main
 
-// type Server struct{
-// 	accountClient *account.Client
-// 	catalogClient *catalog.Client
-// 	orderClient   *order.Client
-// }
+import "github.com/99designs/gqlgen/graphql"
+
+type Server struct {
+	// 	accountClient *account.Client
+	// 	catalogClient *catalog.Client
+	// 	orderClient   *order.Client
+}
 
 func NewGraphQlServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
 	accountClient, err := account.NewClient(accountUrl)
@@ -14,11 +16,45 @@ func NewGraphQlServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) 
 
 	catalogClient, err := catalog.NewClient(catalogUrl)
 	if err != nil {
+		accountClient.Close()
 		return nil, err
 	}
 
 	orderClient, err := order.NewClient(orderUrl)
 	if err != nil {
+		accountClient.Close()
+		catalogClient.Close()
 		return nil, err
 	}
+
+	return &Server{
+		accountClient,
+		catalogClient,
+		orderClient,
+	}, nil
+}
+
+func (s *Server) Mutation() MutationResolver {
+	return &mutationResolver{
+		server: s,
+	}
+}
+
+func (s *Server) Query() QueryResolver {
+	return &queryResolver{
+		server: s,
+	}
+}
+
+func (s *Server) Account() AccountResolver {
+	return &accountResolver{
+		server: s,
+	}
+}
+
+func (s *Server) toExecutableSchema() graphql.ExecutableSchema {
+	return NewExecutableSchema{Config
+	{
+		Resolvers: s,
+	}}
 }
