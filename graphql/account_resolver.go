07e04fc37a92d68ad6d9830cae43e8ac2f81f382
KolManis <nikola.ilyin03@yandex.ru>
2026-01-06 17:43:34 +0500

    feat: реализовал основной GraphQL сервер с конфигурацией, playground и обработкой запросов

--- graphql/account_resolver.go
+++ graphql/account_resolver.go
@@ -1,1 +1,10 @@
 package main
+
+type accountResolver struct {
+	server *Server
+}
+
+// из generated.go  AccountResolver interface
+// func(r *accountResolver) Orders(ctx contex.Context, obj *Account) ([]*Order, error) {
+
+// }
--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -1,19 +1,17 @@
 package main
 
-import "context"
-
 type mutatoinResolver struct {
 	server *Server
 }
 
-func (r *mutatoinResolver) createAccount(ctx context.Context, in AccountInput) (*Account, error) {
+// func (r *mutatoinResolver) createAccount(ctx context.Context, in AccountInput) (*Account, error) {
 
-}
+// }
 
-func (r *mutatoinResolver) createProduct(ctx context.Context, in ProductInput) (*Product, error) {
+// func (r *mutatoinResolver) createProduct(ctx context.Context, in ProductInput) (*Product, error) {
 
-}
+// }
 
-func (r *mutatoinResolver) createOrder(ctx context.Context, in OrderInput) (*Order, error) {
+// func (r *mutatoinResolver) createOrder(ctx context.Context, in OrderInput) (*Order, error) {
 
-}
+// }
