396abb3014a9727ef7fc7b5f4112118faf5c700a
KolManis <nikola.ilyin03@yandex.ru>
2026-01-06 17:00:49 +0500

    feat: заглушки Mutation

--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -1,1 +1,19 @@
 package main
+
+import "context"
+
+type mutatoinResolver struct {
+	server *Server
+}
+
+func (r *mutatoinResolver) createAccount(ctx context.Context, in AccountInput) (*Account, error) {
+
+}
+
+func (r *mutatoinResolver) createProduct(ctx context.Context, in ProductInput) (*Product, error) {
+
+}
+
+func (r *mutatoinResolver) createOrder(ctx context.Context, in OrderInput) (*Order, error) {
+
+}
