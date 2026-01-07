43e241857af27ee4638285aa496f312f7cbe29f3
KolManis <nikola.ilyin03@yandex.ru>
2026-01-07 13:36:42 +0500

    feat: добавлен основной файл сервиса аккаунтов с PostgreSQL репозиторием и gRPC сервером на порту 8080

---
+++ account/cmd/account/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"retry"
+	"time"
+
+	"github.com/KolManis/go-grpc-graphql-microservices/account"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	DatabaseURL string `envconfig:"DATABASE_URL"`
+}
+
+func main() {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var r account.Repository
+	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
+		r, err = account.NewPostgrersRepository(cfg.DatabaseURL)
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	})
+	defer r.Close()
+
+	log.Print("Listenin on port 8080...")
+	s := account.NewService(r)
+	log.Fatal(account.ListenGRPC(s, 8080))
+}
