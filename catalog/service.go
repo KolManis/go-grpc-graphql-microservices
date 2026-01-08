1e6ded37a3eb692067d73fb140da940590c38851
KolManis <nikola.ilyin03@yandex.ru>
2026-01-08 17:44:12 +0500

    feat(catalog): реализовал PostProduct с генерацией ID через KSUID

--- catalog/service.go
+++ catalog/service.go
@@ -1,6 +1,10 @@
 package catalog
 
-import "context"
+import (
+	"context"
+
+	"github.com/segmentio/ksuid"
+)
 
 type Service interface {
 	PostProduct(ctx context.Context, name, description string, price float64) (*Product, error)
@@ -15,4 +19,41 @@ type Product struct {
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
+}
+
+type catalogService struct {
+	repository Repository
+}
+
+func NewService(r Repository) Service {
+	return &catalogService{r}
+}
+
+func (s *catalogService) PostProduct(ctx context.Context, name, description string, price float64) (*Product, error) {
+	p := &Product{
+		ID:          ksuid.New().String(),
+		Name:        name,
+		Description: description,
+		Price:       price,
+	}
+	if err := s.repository.PutProduct(ctx, *p); err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func (s *catalogService) GetProductByID(ctx context.Context, id string) (*Product, error) {
+
+}
+
+func (s *catalogService) GetProducts(ctx context.Context, skip uint64, take uint64) ([]Product, error) {
+
+}
+
+func (s *catalogService) GetProductsWithIDs(ctx context.Context, ids []string) ([]Product, error) {
+
+}
+
+func (s *catalogService) SearchPrducts(ctx context.Context, query string, skip uint64, take uint64) ([]Product, error) {
+
 }
