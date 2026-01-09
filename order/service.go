d9b531985d48f33143e7c9857675d61136bbcdb4
KolManis <nikola.ilyin03@yandex.ru>
2026-01-09 14:42:45 +0500

    feat(order): добавил сервис заказов с бизнес-логикой расчета суммы и генерацией уникальных ID

--- order/service.go
+++ order/service.go
@@ -1,16 +1,58 @@
 package order
 
-import "time"
+import (
+	"context"
+	"time"
 
-type service interface {
-	PutOrder
-	GetordersForAccount
+	"github.com/segmentio/ksuid"
+)
+
+type Service interface {
+	PostOrder(ctx context.Context, accountID string, products []OrderedProduct) (*Order, error)
+	GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error)
 }
 
 type Order struct {
-	ID         string `json:""`
+	ID         string
 	CreatedAt  time.Time
 	TotalPrice float64
 	AccountID  string
 	Products   []OrderedProduct
+}
+
+type OrderedProduct struct {
+	ID          string
+	Name        string
+	Description string
+	Price       float64
+	Quantity    uint32
+}
+
+type orderService struct {
+	repositoty Repositoty
+}
+
+func NewService(r Repositoty) Service {
+	return &orderService{r}
+}
+
+func (s *orderService) PostOrder(ctx context.Context, accountID string, products []OrderedProduct) (*Order, error) {
+	o := &Order{
+		ID:        ksuid.New().String(),
+		CreatedAt: time.Now().UTC(),
+		AccountID: accountID,
+		Products:  products,
+	}
+	o.TotalPrice = 0.0
+	for _, p := range products {
+		o.TotalPrice += p.Price * float64(p.Quantity)
+	}
+	err := s.repositoty.PutOrder(ctx, *o)
+	if err != nil {
+		return nil, err
+	}
+	return o, nil
+}
+func (s *orderService) GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error) {
+	return s.repositoty.GetOrdersForAccount(ctx, accountID)
 }
